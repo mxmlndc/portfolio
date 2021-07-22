78be8cf7c3a623dbfbf430ebce4955ab419e029a
Maximiliano del Caño <mxmlndc@iMac-de-Maximiliano.local>
2021-07-22 19:10:28 -0300

    new portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,172 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+
+        }
+    })
+}
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance:'20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+sr.reveal('.portfolio__img', {interval: 200})
+
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
+
+
+function popUp() {
+    let brands =[{
+        title: 'Diamond Group',
+        text: 'Casino. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port1.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Diamond Group',
+        },
+        {
+        title: 'Install Ar',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port2.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Install Ar',
+        },
+        {
+        title: 'Rue Littre',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port3.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Rue Littre',
+        },
+        {
+        title: 'BK&A',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port4.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'BK&A',
+        },
+        {
+        title: 'Positivo',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port5.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Positivo',
+        },
+        {
+        title: 'Merex',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port6.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Merex',
+        },
+        {
+        title: 'Urban Weld',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port7.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Urban Weld',
+        },
+        {
+        title: 'Batter',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port8.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Batter',
+        },
+        {
+        title: 'Blue Spoon',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port9.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Blue Spoon',
+        },
+        {
+        title: 'CAFU',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port10.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'CAFU',
+        },
+        {
+        title: 'Los Amargos',
+        text: 'Mantenimiento. The client wants to show a new face with a dinamic and modern image. The idea was use a fun font but the diamond and colors keep it formal',
+        imageUrl: 'assets/img/port11.png',
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: 'Los Amargos',
+        }
+    ];
+
+    for (let i= 0; i < brands.length; i++){
+        Swal.fire({
+        title: brands[i].title,
+        text: brands[i].text,
+        imageUrl: brands[i].imageUrl,
+        imageWidth: 350,
+        imageHeight: 200,
+        imageAlt: brands[i].imageAlt,
+        })
+    }                     
+}
